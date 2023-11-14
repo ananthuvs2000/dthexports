6f5a0bcc2297e52eaae7899efc49ca4a3fe2dec6
PremjithK <premjithkallai@gmail.com>
2023-11-14 12:08:18 +0530

    focus shadow

--- lib/_common_widgets/textformfield.dart
+++ lib/_common_widgets/textformfield.dart
@@ -1,7 +1,6 @@
 import 'package:dth/theme/colors.dart';
 import 'package:dth/theme/layout.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class CustomTextFormField extends StatefulWidget {
   const CustomTextFormField({
@@ -26,60 +25,81 @@ class CustomTextFormField extends StatefulWidget {
 }
 
 class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  final FocusNode _focusNode = FocusNode();
+  late FocusNode focusNode;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    focusNode = FocusNode();
+    focusNode.addListener(() => setState(() {}));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: widget.controller,
-      validator: (value) => widget.validator(value),
-      obscureText: widget.isPassword ?? false,
-      keyboardType: widget.keyboardType ?? TextInputType.text,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      expands: false,
-      focusNode: _focusNode,
-      // Styles
-      style: const TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w500,
-        letterSpacing: 0,
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 100),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(globalBorderRadius),
+        boxShadow: focusNode.hasFocus
+            ? [
+                BoxShadow(
+                  color: primaryColor.withOpacity(0.5),
+                  blurRadius: 10,
+                )
+              ]
+            : null,
       ),
-      textInputAction: TextInputAction.next,
-      decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 1,
-            color: Colors.black.withOpacity(0.15),
-          ),
-          borderRadius: BorderRadius.circular(globalBorderRadius),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: primaryColor,
-          ),
-          borderRadius: BorderRadius.circular(globalBorderRadius),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: primaryColor),
-          borderRadius: BorderRadius.circular(globalBorderRadius),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: formFieldErrorColor),
-          borderRadius: BorderRadius.circular(globalBorderRadius),
-        ),
-        prefixIcon: widget.icon,
-        hintText: widget.text,
-        helperStyle: TextStyle(
-          color: Colors.black.withOpacity(0.25),
-          fontSize: 15,
+      child: TextFormField(
+        controller: widget.controller,
+        validator: (value) => widget.validator(value),
+        obscureText: widget.isPassword ?? false,
+        keyboardType: widget.keyboardType ?? TextInputType.text,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        expands: false,
+        focusNode: focusNode,
+        // Styles
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
           letterSpacing: 0,
-          fontWeight: FontWeight.normal,
         ),
-        filled: true,
-        fillColor: inversePrimaryColor,
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 15,
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1,
+              color: Colors.black.withOpacity(0.15),
+            ),
+            borderRadius: BorderRadius.circular(globalBorderRadius),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: primaryColor,
+            ),
+            borderRadius: BorderRadius.circular(globalBorderRadius),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primaryColor),
+            borderRadius: BorderRadius.circular(globalBorderRadius),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: formFieldErrorColor),
+            borderRadius: BorderRadius.circular(globalBorderRadius),
+          ),
+          prefixIcon: widget.icon,
+          hintText: widget.text,
+          helperStyle: TextStyle(
+            color: Colors.black.withOpacity(0.25),
+            fontSize: 15,
+            letterSpacing: 0,
+            fontWeight: FontWeight.normal,
+          ),
+          filled: true,
+          fillColor: inversePrimaryColor,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 15,
+          ),
         ),
       ),
     );
@@ -89,6 +109,6 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    _focusNode.dispose();
+    focusNode.dispose();
   }
 }
