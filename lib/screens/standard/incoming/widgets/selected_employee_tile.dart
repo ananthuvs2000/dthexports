8ba733446bff4e962a57137786a2ee6bdcb10cdf
ananthuvs2000 <ananthuv009@gmail.com>
2023-11-16 13:47:35 +0530

    slidable tile

--- lib/screens/standard/incoming/widgets/selected_employee_tile.dart
+++ lib/screens/standard/incoming/widgets/selected_employee_tile.dart
@@ -1,7 +1,9 @@
 import 'package:dth/_models/employee_model.dart';
 import 'package:dth/theme/colors.dart';
 import 'package:dth/theme/layout.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class SelecteEmployeeTile extends StatelessWidget {
   const SelecteEmployeeTile({
@@ -14,39 +16,53 @@ class SelecteEmployeeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
-      child: Container(
-        decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: AppColors.primaryColor.withOpacity(0.25),
-                blurRadius: 7,
-                offset: const Offset(-2, 2),
-              )
-            ],
-            borderRadius: BorderRadius.circular(globalBorderRadius),
-            gradient: AppGradients.subDashboardOptionGrad),
-        child: ListTile(
-          minVerticalPadding: 0,
-          // tileColor: AppColors.primaryColor,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(globalBorderRadius)),
-          ),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) {},
+              borderRadius: BorderRadius.circular(globalBorderRadius),
+              icon: CupertinoIcons.trash_fill,
+              backgroundColor: Colors.red.shade600,
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.primaryColor.withOpacity(0.25),
+                  blurRadius: 7,
+                  offset: const Offset(-2, 2),
+                )
+              ],
+              borderRadius: BorderRadius.circular(globalBorderRadius),
+              gradient: AppGradients.subDashboardOptionGrad),
+          child: ListTile(
+            minVerticalPadding: 0,
+            // tileColor: AppColors.primaryColor,
+            shape: const RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.all(Radius.circular(globalBorderRadius)),
+            ),
 
-          title: Text(
-            employee.employeeName,
-            style: _titleStyle,
+            title: Text(
+              employee.employeeName,
+              style: _titleStyle,
+            ),
+            subtitle: Text(
+              employee.employeeCode,
+              style: _subtitleStyle,
+            ),
+            // trailing: IconButton(
+            //     onPressed: () => {},
+            //     icon: const Icon(
+            //       CupertinoIcons.trash_fill,
+            //       color: Colors.white,
+            //       size: 20,
+            //     )),
           ),
-          subtitle: Text(
-            employee.employeeCode,
-            style: _subtitleStyle,
-          ),
-          // trailing: IconButton(
-          //     onPressed: () => {},
-          //     icon: const Icon(
-          //       CupertinoIcons.trash_fill,
-          //       color: Colors.white,
-          //       size: 20,
-          //     )),
         ),
       ),
     );
